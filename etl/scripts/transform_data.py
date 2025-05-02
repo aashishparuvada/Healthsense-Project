4ea29401547658fc7e0fa0d447682e56fbb9254b
aashishparuvada <aashishvardhanp@gmail.com>
2025-05-02 13:24:46 +0530

    bug-fix#01

--- etl/scripts/transform_data.py
+++ etl/scripts/transform_data.py
@@ -10,16 +10,23 @@ def transform_data():
     output_path = 'data/transformed_covid_data.csv'
 
     try:
-        df = pd.read_csv(input_path)
+        df_start = pd.read_csv(input_path)
         logging.info(f"Loaded raw data with shape: {df.shape}")
 
-        # Transformation
-        columns_excluded = ['Province/State', 'Country/Region', 'Lat', 'Long']
-        columns_to_sum = df.columns.difference(columns_excluded)
-        columns_to_sum = [col for col in columns_to_sum if pd.api.types.is_numeric_dtype(df[col])]
-        df['total_infected'] = df[columns_to_sum].sum(axis=1)
-        df = df[columns_excluded + ['total_infected']]
+        # Transformation (I assumed those were cases on that particular date but they are actually appending everyday, 
+        # so the last column would contain aggregate, logic for the same is written below this logic)
 
+        # columns_excluded = ['Province/State', 'Country/Region', 'Lat', 'Long']
+        # columns_to_sum = df.columns.difference(columns_excluded)
+        # columns_to_sum = [col for col in columns_to_sum if pd.api.types.is_numeric_dtype(df[col])]
+        # df['total_infected'] = df[columns_to_sum].sum(axis=1)
+        # df = df[columns_excluded + ['total_infected']]
+
+        columns_included = ['Province/State', 'Country/Region', 'Lat', 'Long']   
+        last_column = df_start.columns[-1]
+        columns_to_select = columns_included + [last_column]
+        df = df_start[columns_to_select].copy()
+        df = df.rename(columns={last_column: 'total_infected'})     
 
         # Normalising column names
         df.columns = [col.strip().lower().replace(' ', '_') for col in df.columns]
